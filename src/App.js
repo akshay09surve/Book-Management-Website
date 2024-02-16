f6b4859367dd2fbfc8f1a198c06e8b5a16def1db
akshay surve <akshaysurve.sv@gmail.com>
2024-02-16 20:00:12 +0530

    Login & Registration project

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Homepage from './components/homepage';
+import Login from './components/login';
+import Register from './components/register';
+
+function App() {
+  return (
+    <>
+    <div className='App'>
+        {/* <Homepage/> */}
+        {/* <Login></Login> */}
+        <Register></Register>
+    </div>
+    </>
+  );
+}
+
+export default App;
